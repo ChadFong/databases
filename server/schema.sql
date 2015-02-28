c31cd0967b0879cdb185b6d412211fc351e925b8
Hack Reactor Students <accounts@hackreactor.com>
2015-02-27 20:03:01 -0800

    Progress on MySQL integration for chatterbox-server/client MySQL database schema complete, node server yet to connect with SQL

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,30 @@
-CREATE DATABASE chat;
-
+DROP DATABASE chat;
+CREATE DATABASE `chat`;
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--   createdAt TIMESTAMP(14), username VARCHAR(255), message VARCHAR(255), roomname VARCHAR(255)
+-- );
+CREATE TABLE `Users` (
+  `idxUsers` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(20),
+  PRIMARY KEY  (`idxUsers`)
 );
-
-/* Create other tables and define schemas for them here! */
-
-
-
-
+CREATE TABLE `Messages` (
+  `idxMessages` INT NOT NULL AUTO_INCREMENT,
+  `createdAt` TIMESTAMP,
+  `user` INT,
+  `rooms` INT,
+  `text` VARCHAR(140),
+  PRIMARY KEY  (`idxMessages`)
+);
+CREATE TABLE `Rooms` (
+  `idxRooms` INT NOT NULL AUTO_INCREMENT,
+  `roomname` VARCHAR(20),
+  PRIMARY KEY  (`idxRooms`)
+);
+ALTER TABLE `Messages` ADD CONSTRAINT `Messages_FK_Users` FOREIGN KEY (`user`) REFERENCES Users(`idxUsers`);
+ALTER TABLE `Messages` ADD CONSTRAINT `Messages_FK_Rooms` FOREIGN KEY (`rooms`) REFERENCES Rooms(`idxRooms`);
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
